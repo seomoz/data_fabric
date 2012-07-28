32cf61d381160d39117ac16a32ca0b5323fa0afe
Kevin Griffin <xgriffin.kevin@gmail.com>
2012-07-27 23:16:17 -0500

    Add tests for with_master, threshold-based switching, and reading settings from yml file.

--- test/connection_test.rb
+++ test/connection_test.rb
@@ -15,6 +15,10 @@ class TheWholeEnchilada < ActiveRecord::Base
   data_fabric :prefix => 'fiveruns', :replicated => true, :shard_by => :city
 end
 
+class ReplicateModel < ActiveRecord::Base
+  data_fabric :replicated => true, :dynamic_toggle => true
+end
+
 class AdapterMock < ActiveRecord::ConnectionAdapters::AbstractAdapter
   # Minimum required to perform a find with no results.
   def columns(table_name, name=nil)
@@ -33,7 +37,7 @@ class AdapterMock < ActiveRecord::ConnectionAdapters::AbstractAdapter
     []
   end
   def tables
-    ["enchiladas", "the_whole_burritos"]
+    ["enchiladas", "the_whole_burritos", "replicate_models"]
   end
   def table_exists?(name)
     true
@@ -62,12 +66,12 @@ class ConnectionTest < MiniTest::Unit::TestCase
   def test_should_install_into_arbase
     assert PrefixModel.methods.map(&:to_s).include?('data_fabric')
   end
-
+  
   def test_prefix_connection_name
     setup_configuration_for PrefixModel, 'prefix_test'
     assert_equal 'prefix_test', PrefixModel.connection.connection_name
   end
-
+  
   def test_shard_connection_name
     setup_configuration_for ShardModel, 'city_austin_test'
     # ensure unset means error
@@ -82,7 +86,7 @@ class ConnectionTest < MiniTest::Unit::TestCase
       ShardModel.connection.connection_name
     end
   end
-
+  
   def test_respond_to_connection_methods
     setup_configuration_for ShardModel, 'city_austin_test'
     DataFabric.activate_shard(:city => 'austin', :category => 'art') do
@@ -91,7 +95,7 @@ class ConnectionTest < MiniTest::Unit::TestCase
       assert !ShardModel.connection.respond_to?(:nonexistent_method)
     end
   end
-
+  
   def test_respond_to_connection_proxy_methods
     setup_configuration_for ShardModel, 'city_austin_test'
     DataFabric.activate_shard(:city => 'austin', :category => 'art') do
@@ -99,18 +103,19 @@ class ConnectionTest < MiniTest::Unit::TestCase
       assert !ShardModel.connection.respond_to?(:nonexistent_method)
     end
   end
-
+  
   def test_enchilada
     setup_configuration_for TheWholeEnchilada, 'fiveruns_city_dallas_test_slave'
     setup_configuration_for TheWholeEnchilada, 'fiveruns_city_dallas_test_master'
+  
     DataFabric.activate_shard :city => :dallas do
       assert_equal 'fiveruns_city_dallas_test_slave', TheWholeEnchilada.connection.connection_name
-
+  
       # Should use the slave
       assert_raises ActiveRecord::RecordNotFound do
         TheWholeEnchilada.find(1)
       end
-
+  
       # Should use the master
       mmmm = TheWholeEnchilada.new
       mmmm.instance_variable_set(:@attributes, { 'id' => 1 })
@@ -119,7 +124,7 @@ class ConnectionTest < MiniTest::Unit::TestCase
       end
       # ...but immediately set it back to default to the slave
       assert_equal 'fiveruns_city_dallas_test_slave', TheWholeEnchilada.connection.connection_name
-
+  
       # Should use the master
       TheWholeEnchilada.transaction do
         mmmm.save!
@@ -128,10 +133,44 @@ class ConnectionTest < MiniTest::Unit::TestCase
       TheWholeEnchilada.clear_active_connections!
       TheWholeEnchilada.clear_all_connections!
     end
-
+  
     TheWholeEnchilada.verify_active_connections!
     TheWholeEnchilada.clear_active_connections!
     TheWholeEnchilada.clear_all_connections!
+  end
+  
+  def test_reads_from_slave_when_below_threshold
+    ActiveRecord::Base.configurations = load_database_yml
+    flexmock(DataFabricInterval.instance).should_receive(:threshold).and_return(5)
+    flexmock(DataFabricInterval.instance).should_receive(:seconds_behind).and_return(4)
+    flexmock(DataFabricInterval.instance).should_receive(:check_server?).and_return(true)
+    assert_equal "test_slave", ReplicateModel.find(1).name
+  end
+  
+  def test_reads_from_master_when_above_threshold
+    ActiveRecord::Base.configurations = load_database_yml
+    flexmock(DataFabricInterval.instance).should_receive(:threshold).and_return(1)
+    flexmock(DataFabricInterval.instance).should_receive(:seconds_behind).and_return(4)
+    flexmock(DataFabricInterval.instance).should_receive(:check_server?).and_return(true)
+    assert_equal "test_master", ReplicateModel.find(1).name
+  end
+  
+  def test_with_master_always_goes_to_master
+    ActiveRecord::Base.configurations = load_database_yml
+    flexmock(DataFabricInterval.instance).should_receive(:threshold).and_return(5)
+    flexmock(DataFabricInterval.instance).should_receive(:seconds_behind).and_return(4)
+    flexmock(DataFabricInterval.instance).should_receive(:check_server?).and_return(true)
+    assert_equal "test_master", ReplicateModel.with_master() { ReplicateModel.find(1).name }
+  end
+  
+  def test_it_should_read_the_check_interval_from_the_slave_in_the_yml_file
+    settings = load_database_yml
+    assert_equal settings["test_slave"]["check_interval"], DataFabricInterval.instance.check_interval
+  end
+  
+  def test_it_should_read_the_threshold_from_the_slave_in_the_yml_file
+    settings = load_database_yml
+    assert_equal settings["test_slave"]["delay_threshold"], DataFabricInterval.instance.threshold
   end
 
   private
