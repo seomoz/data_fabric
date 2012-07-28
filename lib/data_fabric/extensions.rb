44b0e35f1978cc4721cb81800a0296f72a51e68d
Kevin Griffin <xgriffin.kevin@gmail.com>
2012-07-27 23:11:35 -0500

    Add "with_master" method on AR::Base-derived classes; add singletons for tracking slave status and delay.

--- lib/data_fabric/extensions.rb
+++ lib/data_fabric/extensions.rb
@@ -1,4 +1,5 @@
 require 'data_fabric/connection_proxy'
+require 'singleton'
 
 class ActiveRecord::ConnectionAdapters::ConnectionHandler
   def clear_active_connections_with_data_fabric!
@@ -8,11 +9,74 @@ class ActiveRecord::ConnectionAdapters::ConnectionHandler
   alias_method_chain :clear_active_connections!, :data_fabric
 end
 
+class DataFabricInterval
+  include Singleton
+  attr_reader :threshold, :check_interval
+  
+  def initialize
+    @last_checked   = Time.now
+    
+    if environment == "test"
+      yml_file = File.join(ROOT_PATH, "test", "database.yml")
+    elsif defined?(Rails)
+      yml_file = Rails.root.join("config", "database.yml")
+    else
+      yml_file = File.join(RAILS_ROOT, "config", "database.yml")
+    end
+  
+    settings = YAML::load(ERB.new(IO.read(yml_file)).result)
+    
+    @check_interval = (settings["#{environment}_slave"] && settings["#{environment}_slave"]["check_interval"])  || 5
+    @threshold      = (settings["#{environment}_slave"] && settings["#{environment}_slave"]["delay_threshold"]) || 5
+  end
+  
+  def check_server?
+    time = Time.now
+    if time > @last_checked + @check_interval
+      @last_checked = time
+      return true
+    end
+    false
+  end
+  
+  def environment
+    (defined?(Rails) && Rails.env) || ENV["RAILS_ENV"] || "test"
+  end
+  
+  def seconds_behind
+    return 0 unless ActiveRecord::Base.connection.adapter_name =~ /mysql/
+    4
+    # result = connection.execute "SHOW SLAVE STATUS;"
+  end
+  
+  def behind?
+    seconds_behind > threshold
+  end
+end
+
+class DataFabricStatus
+  include Singleton
+  
+  def initialize
+    @master = false
+  end
+  
+  def master?
+    @master
+  end
+    
+  def update_status
+    return unless DataFabricInterval.instance.check_server?
+    @master = DataFabricInterval.instance.behind?
+  end
+end
+
 module DataFabric
   module Extensions
+        
     def self.included(model)
       DataFabric.logger.info { "Loading data_fabric #{DataFabric::Version::STRING} with ActiveRecord #{ActiveRecord::VERSION::STRING}" }
-
+            
       # Wire up ActiveRecord::Base
       model.extend ClassMethods
       ConnectionProxy.shard_pools = {}
@@ -23,6 +87,10 @@ module DataFabric
       def data_fabric(options)
         DataFabric.logger.info { "Creating data_fabric proxy for class #{name}" }
         connection_handler.connection_pools[name] = PoolProxy.new(ConnectionProxy.new(self, options))
+      end
+      
+      def with_master(&block)
+        connection_handler.connection_pools[name].connection.with_master(&block)
       end
     end
   end
