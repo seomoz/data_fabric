5a2418e9198b9c6ce83cca42f0edde34c629c36c
Kevin Griffin <xgriffin.kevin@gmail.com>
2012-07-30 19:18:16 -0500

    Move tests for singletons to their own files.

--- test/connection_test.rb
+++ test/connection_test.rb
@@ -15,10 +15,6 @@ class TheWholeEnchilada < ActiveRecord::Base
   data_fabric :prefix => 'fiveruns', :replicated => true, :shard_by => :city
 end
 
-class ReplicateModel < ActiveRecord::Base
-  data_fabric :replicated => true, :dynamic_toggle => true
-end
-
 class NormalModel < ActiveRecord::Base
 end
 
@@ -141,54 +137,7 @@ class ConnectionTest < MiniTest::Unit::TestCase
     TheWholeEnchilada.clear_active_connections!
     TheWholeEnchilada.clear_all_connections!
   end
-  
-  def test_reads_from_slave_when_below_threshold
-    ActiveRecord::Base.configurations = load_database_yml
-    flexmock(DataFabricInterval.instance).should_receive(:threshold).and_return(5)
-    flexmock(DataFabricInterval.instance).should_receive(:seconds_behind).and_return(4)
-    flexmock(DataFabricInterval.instance).should_receive(:check_server?).and_return(true)
-    assert_equal "test_slave", ReplicateModel.find(1).name
-  end
-  
-  def test_reads_from_master_when_above_threshold
-    ActiveRecord::Base.configurations = load_database_yml
-    flexmock(DataFabricInterval.instance).should_receive(:threshold).and_return(1)
-    flexmock(DataFabricInterval.instance).should_receive(:seconds_behind).and_return(4)
-    flexmock(DataFabricInterval.instance).should_receive(:check_server?).and_return(true)
-    assert_equal "test_master", ReplicateModel.find(1).name
-  end
-  
-  def test_with_master_always_goes_to_master
-    ActiveRecord::Base.configurations = load_database_yml
-    flexmock(DataFabricInterval.instance).should_receive(:threshold).and_return(5)
-    flexmock(DataFabricInterval.instance).should_receive(:seconds_behind).and_return(4)
-    flexmock(DataFabricInterval.instance).should_receive(:check_server?).and_return(true)
-    assert_equal "test_master", ReplicateModel.with_master() { ReplicateModel.find(1).name }
-  end
-  
-  def test_seconds_behind_should_return_no_lag_for_an_improper_setup
-    ActiveRecord::Base.configurations = load_database_yml
-    assert_equal 0, DataFabricInterval.instance.seconds_behind
-  end
-  
-  def test_it_should_read_the_check_interval_from_the_slave_in_the_yml_file
-    settings = load_database_yml
-    assert_equal settings["test_slave"]["check_interval"], DataFabricInterval.instance.check_interval
-  end
-  
-  def test_it_should_read_the_threshold_from_the_slave_in_the_yml_file
-    settings = load_database_yml
-    assert_equal settings["test_slave"]["delay_threshold"], DataFabricInterval.instance.threshold
-  end
-  
-  def test_it_should_not_change_the_connection_for_non_replicated_classes
-    ActiveRecord::Base.configurations = load_database_yml
-    ActiveRecord::Base.establish_connection :test_master
-    assert NormalModel.first.name =~ /master/
-    DataFabricInterval.instance.seconds_behind
-    assert NormalModel.first.name =~ /master/
-  end
-  
+    
   private
 
   def setup_configuration_for(clazz, name)
