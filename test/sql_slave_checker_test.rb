77923de5314d7a716f94c08b9cb6459d920d9811
Kevin Griffin <xgriffin.kevin@gmail.com>
2012-09-05 11:35:35 -0500

    Set "slave" database before testing.

--- test/sql_slave_checker_test.rb
+++ test/sql_slave_checker_test.rb
@@ -3,15 +3,16 @@ require 'test/unit'
 
 class SQLSlaveCheckerTest < Test::Unit::TestCase
   def setup
-    @slave_checker = DataFabricDynamicSwitching::SQLSlaveChecker.new("test_slave")
+    @slave_checker = DataFabric::DynamicSwitching::SQLSlaveChecker.new("test_slave")
+    ActiveRecord::Base.establish_connection :test_master
+    @slave_checker.slave_connection = ActiveRecord::Base.connection
   end
-  
+
   def test_seconds_behind_should_return_no_lag_for_an_improper_setup
     assert_equal 0, @slave_checker.seconds_behind
   end
-  
-  def test_it_should_not_change_the_connection_for_non_replicated_classes  
-    ActiveRecord::Base.establish_connection :test_master
+
+  def test_it_should_not_change_the_connection_for_non_replicated_classes
     assert NormalModel.first.name =~ /master/
     @slave_checker.seconds_behind
     assert NormalModel.first.name =~ /master/
